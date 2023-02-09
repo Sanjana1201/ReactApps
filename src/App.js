43d14557f949d50f4a642a14e1e48a904c2c3609
Sanjana1201 <choudhary.sanjana01@gmail.com>
2023-02-09 17:53:59 +0530

    Add clickCounter_Practice1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import NotificationSection from './components/Assignment1/index.js';
-import TechnologySection from './components/Assignment2/index.js';
+// import TechnologySection from './components/Assignment2/index.js';
+import ClickCounter from './components/Practice1_clockCounter/index/index.js';
 
 function App() {
   return (
     <div>
       {/* <NotificationSection /> */}
-      <TechnologySection />
+      {/* <TechnologySection /> */}
+      <ClickCounter />
     </div>
     
   );
