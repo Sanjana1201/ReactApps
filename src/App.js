4e18d7712693d1e88d3c04a244afc51dbc2a8348
Sanjana1201 <choudhary.sanjana01@gmail.com>
2023-02-10 10:59:20 +0530

    add assignment3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // import NotificationSection from './components/Assignment1/index.js';
 // import TechnologySection from './components/Assignment2/index.js';
-import ClickCounter from './components/Practice1_clockCounter/index/index.js';
+// import ClickCounter from './components/Practice1_clockCounter/index/index.js';
+import LightDarkMode from './components/Assignment3-Light&DarkMode/index/index.js';
+
 
 function App() {
   return (
     <div>
       {/* <NotificationSection /> */}
       {/* <TechnologySection /> */}
-      <ClickCounter />
+      {/* <ClickCounter /> */}
+      <LightDarkMode />
     </div>
     
   );
